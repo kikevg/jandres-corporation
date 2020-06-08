02f5b4e36c26575abe12ffb3d2edbd545ce5bee8
kike <kikevigil.dev@gmail.com>
2020-06-07 18:00:41 -0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector('.navbar');
+const section1 = document.querySelector('.section-1');
+
+window.onscroll = (e) => {
+    if (scrollY > 20) {
+        navbar.classList.add('bg-white');
+        navbar.classList.add('shadow-sm');
+        navbar.classList.remove('border-bottom');
+    } else {
+        navbar.classList.remove('bg-white');
+        navbar.classList.remove('shadow-sm');
+    }
+
+    section1.style.transform = 'translateY(' + scrollY / 2 + 'px)';
+}
+
+AOS.init();
